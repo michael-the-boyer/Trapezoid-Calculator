69312447251a3af32ac11327ea643791e662d32e
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-17 17:49:43 +0000

    Added js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('button').addEventListener('click', calculate)
+
+let a = 0
+let b = 0
+let h = 0
+let unit = "unit"
+
+function calculate () {
+
+ a = document.getElementById('a').value
+ b = document.getElementById('b').value
+ h = document.getElementById('h').value
+
+ unit = document.getElementById('unit').value
+
+let area = (a+b)*h/2
+
+ alert("The area of the trapezoid would be " + area + " " + unit + "(s)")
+}
